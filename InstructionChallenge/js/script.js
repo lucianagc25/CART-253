0f41cd52e84cbd4a9cfe12421c44de5dcbac409b
lucianagc25 <luci2825.garcia@gmail.com>
2026-09-16 21:14:42 -0400

    challengefinished

--- InstructionChallenge/js/script.js
+++ InstructionChallenge/js/script.js
@@ -12,7 +12,8 @@
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
 function setup() {
-
+ createCanvas(600, 600);
+    background(0,20,50);
 }
 
 
@@ -21,4 +22,81 @@ function setup() {
 */
 function draw() {
 
+    background(0,20,50);
+   
+
+  cone1();
+
+  cone2();
+
+  cone3();
+
+
+
+
+
+
+  
+
+
 }
+
+
+function cone1() {
+    
+    fill(255, 229, 204);
+    triangle(400, 200, 450, 200, 425, 250);
+
+    fill(51,153, 255);
+    ellipse(430,170, 70, 60);
+   
+    
+    
+//pink circle
+    fill(255,204,209);
+    ellipse(450,187,60, 50);
+    
+//green circle
+    fill(204,255,153);
+    ellipse(400,180,60, 50);
+    
+
+    fill(255, 229, 204);
+    triangle(150, 200, 200, 200, 175, 250);
+}
+
+
+function cone2() {
+
+    fill(255, 229, 204);
+    triangle(150, 200, 200, 200, 175, 250);
+
+    
+    fill(255,255, 204);
+    ellipse(200,200, 50, 50);
+   
+    
+    
+//pink circle
+    fill(255,204,209);
+    ellipse(180,165,60, 50);
+    
+//green circle
+    fill(204,255,153);
+    ellipse(155,180,60, 50);
+}
+
+function cone3() {
+
+    rect(225, 300, 20, 80)
+
+    rect(350, 300, 20, 80)
+
+    ellipse(300, 450, 400, 50);
+
+
+
+
+
+}
+
